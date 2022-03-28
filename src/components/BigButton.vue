4ff0b9d7e94e32a0dc93ae303d2dc093b780fb8e
倪唯 <yuanyi_qingmo@126.com>
2022-03-28 22:16:58 +0800

    3.28

---
+++ src/components/BigButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="main-btn"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    #main-btn {
+        position: absolute;
+        margin: 0 50%;
+        top: 30vh;
+        width: 30vw;
+        height: 30vw;
+        background-color: aqua;
+        // background: url("../assets/click/button.png") no-repeat center center;
+        // background-size: cover;
+        // background-attachment: fixed;
+        border-radius: 999px;
+    }
+</style>
--- src/views/Open.vue
+++ src/views/Open.vue
@@ -1,14 +1,19 @@
 <template>
     <div id="homepage">
-        <div v-show="currentPage == 1" id="background" @click="nextPage"></div>
-        <div v-show="currentPage == 2" id="normal">
-            <router-view />
-        </div>
+        <transition >
+            <div v-if="currentPage == 1" id="background" @click="nextPage" key="first-page"></div>
+            <div v-if="currentPage == 2" id="normal" key="second-page">
+                <big-button></big-button>
+                <img src="../assets/click/arrows.png" id="arrows"/>
+            </div>
+        </transition>
     </div>
 </template>
 
 <script>
+import BigButton from '../components/BigButton.vue';
 export default {
+    components: { BigButton },
     data(){
         return{
             currentPage:1,
@@ -23,10 +28,55 @@ export default {
 </script>
 
 <style lang="scss">
+// 动画
+// .v-enter,.v-leave-to {
+//     opacity: 0;
+//     transform: translateX(50Px);
+// }
+// .v-enter-to, .v-leave {
+//     opacity: 0.4;
+//     transform: translateX(0);
+// }
+// .v-enter-active, .v-leave-active {
+//     transform: all 8s ease;
+// }
+
 #homepage{
     width: 100%;
     height: 100%;
 }
+
+#arrows{
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: 0 50%;
+    transform: translate(-50%, -50%);
+    width: 100vw;
+    height: 100vw;
+    animation-name: beat;
+    /*动画名称*/
+    animation-duration: 1s;
+    /*设置秒数*/
+    animation-timing-function: linear;
+    /*速度曲线*/
+    animation-iteration-count: infinite;
+    /*播放次数*/
+    animation-direction: alternate;
+    /*逆向播放*/
+    animation-play-state: running;
+    /*正在运行*/
+}
+
+@keyframes beat {
+        0% {
+            top:60vh;
+        }
+        100% {
+            top:62vh;
+        }
+    }
+
 
 #background {
     width: 100%;
@@ -37,6 +87,8 @@ export default {
 }
 
 #normal {
+    display: flex;
+    justify-content: center;
     width: 100%;
     height: 100%;
     background: url("../assets/background.png") no-repeat center center;
