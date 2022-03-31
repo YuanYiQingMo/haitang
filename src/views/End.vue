14950aaa9b8b6254dcd7c0a91be6efba6cd9476b
Sisiliu312 <1579131893@qq.com>
2022-03-31 16:31:49 +0800

    save

--- src/views/End.vue
+++ src/views/End.vue
@@ -14,13 +14,12 @@
 export default {
     data() {
         return {
-            savedimg:false,
+            savedimg:true,
         }
     },
     beforeCreate(){
         let card = this.$route.query.card
         console.log(card)
-
     },
     methods: {
         tosave:function(){
@@ -42,6 +41,7 @@ export default {
 #save{
     width: 100%;
     height: 100%;
+    font-size: 1.2rem;
 }
 #background {
     width: 100%;
@@ -54,49 +54,52 @@ export default {
     position: absolute;
     width: 84%;
     height: 90%;
-    margin:40px 30px;
-    /* // display: flex;
+    margin:10% 7%;
+    // display: flex;
     // flex-direction: column;
-    // justify-content: space-between; */
-    align-items: center;
+    // justify-content: space-between; 
+    // align-items: center;
 }
 .collection{
-    height: 27px;
-    width: 100px;
-    padding:3% 6%;
+    // height: 7%;
+    width: 40%;
+    text-align:center;
     background: url("../assets/card/collection.png") no-repeat center center ;
     align-self:flex-end;
     border-radius: 5px;
-    font-size: 1.2rem;
+    padding: 3% 0;
+    // font-size: 50%;
     color:#b1898d;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 .picture{
     height: 80%;
     width: 100%;
-    margin:20px 7px 20px 7px;
+    margin:7% 2px 7% 2%;
     border-radius: 5px;
     opacity: 0.7;
 }
 .saveButton{
-    height: 27px;
-    width: 100px;
-    padding:3% 6%;
+    // height: 7%;
+    width: 50%;
+    padding: 3% 0;
     background: url("../assets/card/save.png") no-repeat center center;
     border-radius: 5px;
-    font-size: 1.25rem;
+    // font-size: 1.25rem;
+    text-align:center;
     color:#b1898d;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 .saved{
     position: absolute;
     top:45%;
-    height: 27px;
-    width: 120px;
-    padding:3% 6%;
+    height: 4%;
+    width: 50%;
+    padding:3% 0;
     background: url("../assets/card/saved.png") no-repeat center center;
     border-radius: 7px;
-    font-size: 1.25rem;
+    // font-size: 38%;
+    text-align:center;
     color:#b1898d;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     box-shadow: 5px 5px 15px rgba(41, 40, 40, 0.3);
